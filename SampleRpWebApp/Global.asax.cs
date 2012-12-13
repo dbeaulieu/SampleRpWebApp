1943a8eb51bdeac8fc3c248532b38a96833b5e13
Dan Beaulieu <dbeaulieu03@gmail.com>
2012-12-13 09:48:20 -0500

    Added STS reference

--- SampleRpWebApp/Global.asax.cs
+++ SampleRpWebApp/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -23,6 +25,9 @@ namespace SampleRpWebApp
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Name;
+            
         }
     }
 }
